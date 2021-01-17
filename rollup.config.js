f805a7c4b634ba07f43016051f42bf2fac18e2eb
Julian Burgess <aubergene@gmail.com>
2021-01-17 00:10:50 +0000

    Dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,40 @@
-import buble from 'rollup-plugin-buble';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import buble from "@rollup/plugin-buble";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 
-export default [{
-  input: 'src/index.js',
-  external: ['adaptive-bezier-curve', 'adaptive-quadractive-curve', 'transformation-matrix'],
-  plugins: [
-    buble()
-  ],
-  output: {
-    file: 'dist/canvas-polyline.es.js',
-    format: 'es',
-    sourcemap: true
-  }
-}, {
-  input: 'src/index.js',
-  plugins: [
-    buble(),
-    resolve(),
-    commonjs()
-  ],
-  output:
+export default [
   {
-    file: 'dist/canvas-polyline.umd.js',
-    name: 'CanvasPolyline',
-    format: 'umd',
-    sourcemap: true
-  }
-}, {
-  input: 'src/index.js',
-  plugins: [
-    buble(),
-    resolve(),
-    commonjs(),
-    terser()
-  ],
-  output:
+    input: "src/index.js",
+    external: [
+      "adaptive-bezier-curve",
+      "adaptive-quadractive-curve",
+      "transformation-matrix",
+    ],
+    plugins: [buble()],
+    output: {
+      file: "dist/canvas-polyline.es.js",
+      format: "es",
+      sourcemap: true,
+    },
+  },
   {
-    file: 'dist/canvas-polyline.umd.min.js',
-    name: 'CanvasPolyline',
-    format: 'umd'
-  }
-}]
+    input: "src/index.js",
+    plugins: [buble(), resolve(), commonjs()],
+    output: {
+      file: "dist/canvas-polyline.umd.js",
+      name: "CanvasPolyline",
+      format: "umd",
+      sourcemap: true,
+    },
+  },
+  {
+    input: "src/index.js",
+    plugins: [buble(), resolve(), commonjs(), terser()],
+    output: {
+      file: "dist/canvas-polyline.umd.min.js",
+      name: "CanvasPolyline",
+      format: "umd",
+    },
+  },
+];
